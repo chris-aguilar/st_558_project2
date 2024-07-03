3d6f3baa52b0040392ea1b80bfe3e01679c7f76a
Chris Aguilar <aguilar.christopher.a@gmail.com>
2024-07-03 12:42:40 -0400

    feature: added game info helper function that only requires year and has a default of current year, but also allows for optional parameters year, round, game, team, complete, and live

--- helpers.R
+++ helpers.R
@@ -12,7 +12,7 @@ library(dplyr)
 
 
 # Endpoint 1: team data containing team debut year, abbrevation, id, logo and full name
-
+# optional parameters: Team ID {team}, Debut Year {year}
 get_teams <- function(...) {
   
   url <- 'https://api.squiggle.com.au/'
@@ -31,4 +31,23 @@ get_teams <- function(...) {
   parsed_result$teams |> tibble::as_tibble()
 }
 
-
+# Endpoint 2: info about one or more games
+# optional parameters: year, round, game id, team id, complete, live
+get_games <- function(year = NULL, ...) {
+  
+  url <- 'https://api.squiggle.com.au/'
+  q <- 'games'
+  if(is.null(year)) year <- format(Sys.Date(), "%Y") |> as.integer()
+  
+  # search parameters
+  query <- list(q = q, year = year, ...)
+  
+  # api call
+  api_return <- GET(url = url, query = query)
+  
+  # parsing results
+  parsed_result <- fromJSON(rawToChar(api_return$content))
+  
+  # putting results into tibble for analysis
+  parsed_result$games |> tibble::as_tibble()
+}
